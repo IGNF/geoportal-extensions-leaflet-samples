e7127505d94f781be0ef0099b6edec1a3b30a61e
lowzonenose <jpbazonnais@gmail.com>
2022-06-20 13:41:37 +0200

    clean gh-pages

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 
-  "/geoportal-extensions-leaflet-samples/" : "",
-  configureWebpack: config => {},
-  chainWebpack: config => {},
-}
